7287278a61775bfb2c94c7a179a1a30b7c8add10
Marjona Rakhmatillaeva <70479211+Marjonagit@users.noreply.github.com>
2024-01-11 04:07:45 +0500

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'savedata.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyFirstApp());
+}
+
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Saving Data Firestore',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: SaveDataScreen(),
+  }
+}
